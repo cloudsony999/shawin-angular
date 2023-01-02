86a9c32e77c715b0b756e8faf471abc598f1241d
cloudsony999 <cloudsony999@gmail.com>
2023-01-02 17:49:13 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'shawin-angular';
+ x = 'shawin created an angular application which ';
 }
